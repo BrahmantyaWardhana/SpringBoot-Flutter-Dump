d6fc0dea2cd037a905bc25162515a9ba8f75c468
BrahmantyaWardhana <brahmantya.wardhana.billy@gmail.com>
2025-08-30 10:57:48 +0700

    spring boot init

---
+++ backend-springboot/experimentation/src/test/java/com/bwardhana/experimentation/ExperimentationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bwardhana.experimentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExperimentationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
