d608ce10854056e960b3b2a50e3b60c66e5cce1f
BrahmantyaWardhana <brahmantya.wardhana.billy@gmail.com>
2025-09-02 07:57:26 +0700

    successful db write

---
+++ backend-springboot/database/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO widgets (id, name, purpose) VALUES
+(1, 'Widget A', 'Used for testing'),
+(2, 'Widget B', 'Used for entertainment'),
+(3, 'Widget C', 'Used for productivity'),
+(4, 'Widget D', 'Used for activities'),
+(5, 'Widget E', 'Used for well-being');
