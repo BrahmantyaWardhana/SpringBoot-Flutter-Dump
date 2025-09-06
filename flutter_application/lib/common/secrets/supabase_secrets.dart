837f893dd0acf51dd0862316f727709b572012d5
BrahmantyaWardhana <brahmantya.wardhana.billy@gmail.com>
2025-09-06 17:01:14 +0700

    supabase setup

---
+++ flutter_application/lib/common/secrets/supabase_secrets.dart
@@ -0,0 +1,5 @@
+class SupabaseSecrets {
+  static const supabaseUrl = 'https://mvztpdyqmcvewlzeozym.supabase.co';
+  static const supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12enRwZHlxbWN2ZXdsemVvenltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcxMzQ4MTEsImV4cCI6MjA3MjcxMDgxMX0.gcOLfuTSw9IjPCWribuahmFK7Knrd0Z_UbAAOdbW47w';
+}
--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/common/secrets/supabase_secrets.dart';
 import 'package:flutter_application/common/theme/theme.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'routes.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final supabase = await Supabase.initialize(
+    url: SupabaseSecrets.supabaseUrl,
+    anonKey: SupabaseSecrets.supabaseAnonKey,
+  );
   runApp(const MyApp());
 }
 
