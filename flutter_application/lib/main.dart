5c2d1767fdee5ee2a857f28128bc0e73637935df
BrahmantyaWardhana <brahmantya.wardhana.billy@gmail.com>
2025-09-08 15:03:32 +0700

    add: added multibloc provider in main

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application/common/secrets/supabase_secrets.dart';
 import 'package:flutter_application/common/theme/theme.dart';
+import 'package:flutter_application/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:flutter_application/features/auth/data/repository/auth_repository_impl.dart';
+import 'package:flutter_application/features/auth/domain/usecase/user_signup.dart';
+import 'package:flutter_application/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'routes.dart';
 
 void main() async {
@@ -11,7 +15,22 @@ void main() async {
     url: SupabaseSecrets.supabaseUrl,
     anonKey: SupabaseSecrets.supabaseAnonKey,
   );
-  runApp(const MyApp());
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => AuthBloc(
+            userSignup: UserSignup(
+              authRepository: AuthRepositoryImpl(
+                AuthRemoteDataSourceImpl(supabase.client),
+              ),
+            ),
+          ),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
