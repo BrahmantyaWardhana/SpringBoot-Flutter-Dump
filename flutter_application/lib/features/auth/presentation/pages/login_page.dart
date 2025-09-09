d76e8b25b9bd9f895dc696b90a5b6c47225689c2
BrahmantyaWardhana <brahmantya.wardhana.billy@gmail.com>
2025-09-09 15:16:31 +0700

    add: login logic (pre testing)

--- flutter_application/lib/features/auth/presentation/pages/login_page.dart
+++ flutter_application/lib/features/auth/presentation/pages/login_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
+import 'package:flutter_application/common/error/snackbar_error.dart';
+import 'package:flutter_application/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:flutter_application/features/auth/presentation/widgets/auth_header.dart';
 import 'package:flutter_application/features/auth/presentation/widgets/auth_submit_button.dart';
 import 'package:flutter_application/features/auth/presentation/widgets/auth_login_form.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 class LoginPage extends StatefulWidget {
@@ -33,71 +36,79 @@ class _LoginPageState extends State<LoginPage> {
     FocusScope.of(context).unfocus();
 
     if (_formKey.currentState?.validate() != true) return;
-
-    setState(() => _submitting = true);
-    try {
-      // TODO: Call your signup API here using:
-      // _usernameCtrl.text, _passwordCtrl.text
-      // If success -> navigate
-      await Future.delayed(const Duration(milliseconds: 300));
-      if (!mounted) return;
-      context.go('/');
-    } finally {
-      if (!mounted) return;
-      setState(() => _submitting = false);
-    }
+    context.read<AuthBloc>().add(
+      AuthLogin(
+        email: _emailCtrl.text.trim(),
+        password: _passwordCtrl.text.trim(),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.fromLTRB(20, 16, 20, 24),
-          child: Column(
-            children: [
-              const SizedBox(height: 8),
-              // header
-              const AuthHeader(),
-              // form container
-              AuthLoginInForm(
-                formKey: _formKey,
-                emailController: _emailCtrl,
-                passwordController: _passwordCtrl,
-                hidePassword: _hidePwd,
-                onTogglePassword: () => setState(() => _hidePwd = !_hidePwd),
-                agreeToTerms: _agreeToTerms,
-                onAgreeToTermsChanged: (v) =>
-                    setState(() => _agreeToTerms = v ?? false),
-                onLoginWithGoogle: () {}, // IMPLEMENT GOOGLE LOGIN
-              ),
-              const SizedBox(height: 28),
-              RichText(
-                text: TextSpan(
-                  text: 'Don\'t have an account? ',
-                  children: [
-                    TextSpan(
-                      text: 'Sign Up',
-                      style: const TextStyle(
-                        color: Color.fromARGB(255, 231, 188, 87),
-                      ),
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () => context.push('/signup'),
-                    ),
-                  ],
+    return BlocListener<AuthBloc, AuthState>(
+      listener: (context, state) {
+        if (state is AuthLoading) {
+          setState(() => _submitting = true);
+        } else {
+          if (_submitting) setState(() => _submitting = false);
+        }
+        if (state is AuthSuccess) {
+          context.go('/');
+        }
+        if (state is AuthFailure) {
+          snackbarError(context, state.message);
+        }
+      },
+      child: Scaffold(
+        body: SafeArea(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.fromLTRB(20, 16, 20, 24),
+            child: Column(
+              children: [
+                const SizedBox(height: 8),
+                // header
+                const AuthHeader(),
+                // form container
+                AuthLoginInForm(
+                  formKey: _formKey,
+                  emailController: _emailCtrl,
+                  passwordController: _passwordCtrl,
+                  hidePassword: _hidePwd,
+                  onTogglePassword: () => setState(() => _hidePwd = !_hidePwd),
+                  agreeToTerms: _agreeToTerms,
+                  onAgreeToTermsChanged: (v) =>
+                      setState(() => _agreeToTerms = v ?? false),
+                  onLoginWithGoogle: () {}, // IMPLEMENT GOOGLE LOGIN
                 ),
-              ),
-            ],
+                const SizedBox(height: 28),
+                RichText(
+                  text: TextSpan(
+                    text: 'Don\'t have an account? ',
+                    children: [
+                      TextSpan(
+                        text: 'Sign Up',
+                        style: const TextStyle(
+                          color: Color.fromARGB(255, 231, 188, 87),
+                        ),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () => context.push('/signup'),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
-      ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.fromLTRB(20, 10, 20, 20),
-        child: SizedBox(
-          height: 56,
-          child: AuthSubmitButton(
-            isLoading: _submitting,
-            onPressed: _handleSubmit,
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.fromLTRB(20, 10, 20, 20),
+          child: SizedBox(
+            height: 56,
+            child: AuthSubmitButton(
+              isLoading: _submitting,
+              onPressed: _handleSubmit,
+            ),
           ),
         ),
       ),
