e920035c4fb55d25d8b7f049011a0d5ead528d23
BrahmantyaWardhana <brahmantya.wardhana.billy@gmail.com>
2025-09-18 11:07:04 +0700

    add: added usecase to check for current user

---
+++ flutter_application/lib/features/auth/domain/usecases/current_user.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_application/common/error/failure.dart';
+import 'package:flutter_application/common/usecase/usecase.dart';
+import 'package:flutter_application/features/auth/domain/entities/user.dart';
+import 'package:flutter_application/features/auth/domain/repositories/auth_repository.dart';
+import 'package:fpdart/fpdart.dart';
+
+class CurrentUser implements Usecase<User, NoParams> {
+  final AuthRepository authRepository;
+  CurrentUser(this.authRepository);
+
+  @override
+  Future<Either<Failure, User>> call(NoParams params) async {
+    return await authRepository.currentUser();
+  }
+}
